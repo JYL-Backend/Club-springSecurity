fadded7eb22da8ac3ff0fc87d2241ab59fdaefed
appbutton6 <jylim@bsks.ac.kr>
2021-06-29 14:10:38 +0900

    API서버를 위한 필터

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -10,6 +10,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.zerock.club.security.filter.ApiCheckFilter;
+import org.zerock.club.security.filter.ApiLoginFilter;
+import org.zerock.club.security.handler.ApiLoginFailHandler;
 import org.zerock.club.security.handler.ClubLoginSuccessHandler;
 import org.zerock.club.security.service.ClubUserDetailService;
 
@@ -40,10 +44,30 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
         http.oauth2Login().successHandler(successHandler());
         http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailService);
+
+        http.addFilterBefore(apiCheckFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(apiLoginFilter(),
+                UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
     public ClubLoginSuccessHandler successHandler(){
         return new ClubLoginSuccessHandler(passwordEncoder());
+    }
+
+    @Bean
+    public ApiCheckFilter apiCheckFilter(){
+        return new ApiCheckFilter("/notes/**/*");
+    }
+
+    @Bean
+    public ApiLoginFilter apiLoginFilter() throws Exception {
+        ApiLoginFilter apiLoginFilter = new ApiLoginFilter("/api/login");
+        apiLoginFilter.setAuthenticationManager(authenticationManager());
+
+        apiLoginFilter.setAuthenticationFailureHandler(new ApiLoginFailHandler());
+
+        return apiLoginFilter;
     }
 }
--- src/main/java/org/zerock/club/controller/NoteController.java
+++ src/main/java/org/zerock/club/controller/NoteController.java
@@ -3,13 +3,13 @@ package org.zerock.club.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.club.security.dto.NoteDTO;
 import org.zerock.club.security.service.NoteService;
+
+import java.util.List;
 
 @RestController
 @Log4j2
@@ -19,10 +19,39 @@ public class NoteController {
 
     private final NoteService noteService;
 
+    /* 등록 */
     @PostMapping(value="")
     public ResponseEntity<Long> register(@RequestBody NoteDTO noteDTO){
         Long num = noteService.register(noteDTO);
 
         return new ResponseEntity<>(num, HttpStatus.OK);
+    }
+
+    /* 단 건 조회 */
+    @GetMapping(value = "/{num}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<NoteDTO> read(@PathVariable("num") Long num){
+        return new ResponseEntity<>(noteService.get(num), HttpStatus.OK);
+    }
+
+    /* 리스트 조회 */
+    @GetMapping(value="/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<NoteDTO>> getList(String email){
+        return new ResponseEntity<>(noteService.getAllWithWriter(email), HttpStatus.OK);
+    }
+
+    /* 삭제 */
+    @DeleteMapping(value = "/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> remove(@PathVariable("num") Long num){
+        noteService.remove(num);
+
+        return new ResponseEntity<>("removed", HttpStatus.OK);
+    }
+
+    /* 수정 */
+    @PutMapping(value = "/{num}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> modify(@RequestBody NoteDTO noteDTO){
+        noteService.modify(noteDTO);
+
+        return new ResponseEntity<>("modified", HttpStatus.OK);
     }
 }
